3cb21c75d9a4cf12ab4ba62f1f7d2249113e0109
dfrafelipe30 <33558046+dfrafelipe30@users.noreply.github.com>
2018-05-01 21:11:20 -0500

    Nueva maquina de turing

--- Proyecto de Teoria/Turing/tm4.py
+++ Proyecto de Teoria/Turing/tm4.py
@@ -1,44 +1,44 @@
 from Turing import TM
 import time
 
-#L(M) = {a^ib^ic^i | i > 0 }
+#L(M) = {0^n1^n2 | n > 0 }
 
-states = ['q0','q1','q2','q3','q4','q5','q6']
-alphabet = list("abc")
-tapeAlphabet = list("abcXYZB")
+states = ['q0','q1','q2','q3','q4','q5']
+alphabet = list("012")
+tapeAlphabet = list("01BXY")
 transitions = {
-    ('q0','a'):('q1','X','D'),
-    ('q0','Y'):('q4','Y','D'),
-    ('q1','a'):('q1','Y','D'),
-    ('q1','Y'):('q1','Y','D'),
-    ('q1','b'):('q2','Y','D'),
-    ('q2','b'):('q2','b','D'),
-    ('q2','Z'):('q2','Z','D'),
-    ('q2','c'):('q3','Z','D'),
-    ('q3','a'):('q3','a','I'),
-    ('q3','b'):('q3','b','I'),
-    ('q3','Y'):('q3','Y','I'),
-    ('q3','Z'):('q3','Z','I'),
-    ('q3','X'):('q0','X','D'),
-    ('q4','Y'):('q4','Y','D'),
-    ('q4','Z'):('q4','Z','D'),
+    ('q0','B'):('q0','B','D'),
+    ('q0','0'):('q1','X','D'),
+    ('q0','Y'):('q0','Y','D'),
+    ('q1','B'):('q1','B','D'),
+    ('q1','Y'):('q3','Y','D'),
+    ('q1','1'):('q2','Y','I'),
+    ('q1','0'):('q1','0','D'),
+    ('q2','B'):('q2','B','I'),
+    ('q2','0'):('q2','0','I'),
+    ('q2','Y'):('q2','Y','I'),
+    ('q2','X'):('q0','X','D'),
+    ('q3','Y'):('q3','Y','D'),
+    ('q3','1'):('q2','Y','I'),
     #estados de reject
-    ('q0','B'):('q6','B','D'),
-    ('q0','Y'):('q6','Y','D'),
-    ('q0','b'):('q6','b','D'),
-    ('q0','c'):('q6','c','D'),
-    ('q1','c'):('q6','c','D'),
-    ('q1','Z'):('q6','Z','D'),
-    ('q2','B'):('q6','B','D'),
-    ('q4','Y'):('q4','Y','D'),
+    ('q0','1'):('q5','1','D'),
+    ('q3','2'):('q5','2','D'),
     #estados de aceptacion
-    ('q4','B'):('q5','B','D'),
+    ('q0','2'):('q4','2','D'),
     }
 start = 'q0'
-accept = 'q5'
-reject = 'q6'
+accept = 'q4'
+reject = 'q5'
 
 tm4 = TM(states,alphabet,tapeAlphabet,transitions,start,accept,reject)
 
-w = "abc"
-print tm4.test(w)
+#w = "001112"
+#print tm4.test(w)
+n = 5
+ini = time.time()
+aceptados =  tm4.generateLanguage(n)
+print aceptados 
+fin = time.time()
+print "Tiempo que tarda generateLanguage con n = %d:" %n
+print fin - ini
+
